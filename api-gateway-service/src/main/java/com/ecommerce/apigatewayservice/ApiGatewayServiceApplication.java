ff96c16c7ba88d02c740a340bd65e50709714add
Margareta Galaju <m.galaju@emendis.nl>
2021-10-10 20:43:25 +0300

    Added caching to requests from API Gateway service

--- api-gateway-service/src/main/java/com/ecommerce/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/ecommerce/apigatewayservice/ApiGatewayServiceApplication.java
@@ -2,6 +2,7 @@ package com.ecommerce.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
@@ -10,6 +11,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCaching
 public class ApiGatewayServiceApplication {
 
 	@Bean
--- api-gateway-service/src/main/java/com/ecommerce/apigatewayservice/resources/APIGatewayResource.java
+++ api-gateway-service/src/main/java/com/ecommerce/apigatewayservice/resources/APIGatewayResource.java
@@ -5,6 +5,7 @@ import com.ecommerce.apigatewayservice.models.ProductDetails;
 import com.ecommerce.apigatewayservice.models.ProductReviews;
 import com.ecommerce.apigatewayservice.models.Review;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -24,9 +26,15 @@ public class APIGatewayResource {
     @Autowired
     private WebClient.Builder webClientBuilder;
 
+
     @RequestMapping("/{productId}")
+    @Cacheable("books")
     public ProductDetails getProductDetails(@PathVariable("productId") String productId) {
         ProductDetails productDetails = new ProductDetails("", "", "", "", null);
+
+//        Check caching:
+//        simulateSlowService();
+
         ProductReviews productReviews = restTemplate.getForObject("http://REVIEWS-SERVICE/reviews/lala", ProductReviews.class);
         CatalogItem catalogItem = restTemplate.getForObject("http://CATALOG-SERVICE/catalog/product/my-id", CatalogItem.class);
 
@@ -44,4 +52,14 @@ public class APIGatewayResource {
 
         return productDetails;
     }
+
+//    private void simulateSlowService() {
+//        try {
+//            long time = 3000L;
+//            Thread.sleep(time);
+//        } catch (InterruptedException e) {
+//            throw new IllegalStateException(e);
+//        }
+//    }
+
 }
